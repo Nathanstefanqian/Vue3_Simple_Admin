1fcddcd26f5cc7d246e9214a6be7d8807334b276
NathanQian <1532722889@qq.com>
2023-08-25 11:14:31 +0800

    feat: first commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,48 @@
+<template>
+ <el-form
+    ref="ruleFormRef"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="120px"
+    class="demo-ruleForm"
+  >
+    <el-form-item label="账号" prop="username">
+      <el-input v-model="ruleForm.username" type="password" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input
+        v-model="ruleForm.password"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
+    <el-form-item label="Age" prop="age">
+      <el-input v-model.number="ruleForm.age" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm(ruleFormRef)">Submit</el-button>
+      <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const data = reactive({
+      ruleForm: {
+        username: "",
+        password: ""
+      }
+    })
+    return { ...toRefs(data) };
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
